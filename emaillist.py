1348f8eb18ea09636b652af8aa9b5937a919ecaf
amankumarsingh77 <amankumarsingh7702@gmal.com>
2024-02-06 10:10:42 +0530

    Added email list method

---
+++ emaillist.py
@@ -0,0 +1,9 @@
+
+def get_email():
+    emails =[
+        "2210030344@klh.edu.in",
+        "2210030242@klh.edu.in",
+        "2210030353@klh.edu.in",
+        "2210030119@klh.edu.in"
+    ]
+    return emails
--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from timeconvert import time_from_to
 from dotenv import load_dotenv
 load_dotenv()
 import logging
+from emaillist import  get_email
 
 
 logging.basicConfig(filename='log.txt', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -30,12 +31,12 @@ def send_email(recipient, subject, message):
     sender_email = os.getenv("SMTP_USERNAME")
     sender_password = os.getenv("SMTP_PASSWORD")
 
-    # Create a secure SSL context
+
     context = ssl.create_default_context()
 
     msg = MIMEMultipart()
     msg['From'] = sender_email
-    msg['To'] = recipient
+    msg['To'] = ", ".join(recipient)
     msg['Subject'] = subject
 
     msg.attach(MIMEText(message, 'plain'))
@@ -46,35 +47,31 @@ def send_email(recipient, subject, message):
         server.sendmail(sender_email, recipient, text)
         logging.info("Email Sent")
 
-# Main function
 def main():
-
-    try:
+        logging.info("Program Initiated")
         time_from, time_to= time_from_to()
         assignments = api_request(time_from,time_to)
-        print("Assignments",assignments)
         previous_assignments = load_previous_assignments()
+        try:
+            new_assignments = []
+            for assignment in assignments:
+                # Check if the assignment is new
+                if assignment not in previous_assignments:
+                    new_assignments.append(assignment)
 
+            if new_assignments:
+                recipient_email = get_email()
+                subject = "New KLH Assignment Alert A4"
+                message = ''
+                for assignment in new_assignments:
+                    message += f"New Assignment of {assignment['subject']} . Due Date {assignment['date']}. Click on the link to submit the assignment {assignment['url']} \n"
+                print(message)
+                send_email(recipient_email,subject,message)
 
-        new_assignments = []
-        for assignment in assignments:
-            # Check if the assignment is new
-            if assignment not in previous_assignments:
-                new_assignments.append(assignment)
-
-        if new_assignments:
-            recipient_email = "2210030344@klh.edu.in"
-            subject = "New KLH Assignment Alert A4"
-            message = ''
-            for assignment in new_assignments:
-                message += f"New Assignment of {assignment['subject']}. Must be submitted by {assignment['date']}. Click on the link to submit the assignment {assignment['url']} \n"
-            print(message)
-            send_email(recipient_email,subject,message)
-            # Update the stored assignments
-            updated_assignments = previous_assignments + new_assignments
-            save_assignments(updated_assignments)
-    except Exception as e:
-        logging.error("Error occurred while retrieving assignments: %s", str(e))
+                updated_assignments = previous_assignments + new_assignments
+                save_assignments(updated_assignments)
+        except Exception as e:
+            logging.error("Error occurred while retrieving assignments: %s", str(e))
 
 
 if __name__ == "__main__":
